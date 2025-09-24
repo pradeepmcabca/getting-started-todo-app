f53a24f4b6abcfc91751a00219a2b99e489f7578
pradeepmcabca <pradeep.mcabca@gmail.com>
2025-09-24 22:57:40 +0530

    change place holder for item.

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,10 @@
-const GREETING = 'Hello world!';
+const GREETING = ['Hello world!', 'Hola Mundo!',
+     'Bonjour le monde!', 'Hallo Welt!', 'Ciao mondo!'
+    ][Math.floor(Math.random() * 5)];
+
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETING[Math.floor(Math.random() * GREETING.length)],
     });
 };
